6b466fa2e255c967b511804b1aebfa360a3a2aa8
Brandyux <696brandy@gmail.com>
2021-05-17 19:25:54 -0400

    añadir un boton

--- lib/linktree.dart
+++ lib/linktree.dart
@@ -32,9 +32,10 @@ class LinkTree extends StatelessWidget {
         ButtonLink(
           text: 'Facebook',
         ),
-        Youtube(),
-        Whatsapp(),
-        Whatsapp1(),
+        Youtube(text: 'Youtube'),
+        Whatsapp(text: 'Whatsapp Op.1'),
+        Whatsapp1(text: 'Whatsapp Op.2'),
+        Tiktok(text: 'TikTok'),
 
 //BOTON 01
 
@@ -76,7 +77,7 @@ class ButtonLink extends StatelessWidget {
           //onOpen: _onOpen,
           //    text: "Facebook"),
           child: Text(
-            'Galeria',
+            text,
             style: TextStyle(
                 color: Colors.white, fontFamily: 'Courgette', fontSize: 18),
           ),
@@ -103,7 +104,7 @@ class Youtube extends StatelessWidget {
           color: Colors.red[300],
           padding: EdgeInsets.symmetric(vertical: 28),
           child: Text(
-            'Facebook',
+            text,
             style: TextStyle(
                 color: Colors.white, fontFamily: 'Courgette', fontSize: 18),
           ),
@@ -129,7 +130,7 @@ class Whatsapp extends StatelessWidget {
           color: Colors.cyanAccent[400],
           padding: EdgeInsets.symmetric(vertical: 28),
           child: Text(
-            'Whatsapp Op.1',
+            text,
             style: TextStyle(
                 color: Colors.white, fontFamily: 'Courgette', fontSize: 18),
           ),
@@ -155,12 +156,39 @@ class Whatsapp1 extends StatelessWidget {
           color: Colors.red[300],
           padding: EdgeInsets.symmetric(vertical: 28),
           child: Text(
-            'Whatsapp Op.2  \uf111',
+            text,
             style: TextStyle(
                 color: Colors.white, fontFamily: 'Courgette', fontSize: 18),
           ),
           onPressed: () {
             launch('https://wa.link/0750ku');
+          },
+        ),
+      ),
+    );
+  }
+}
+
+class Tiktok extends StatelessWidget {
+  const Tiktok({Key key, @required this.text}) : super(key: key);
+  final String text;
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 10),
+      child: SizedBox(
+        width: MediaQuery.of(context).size.width * 0.95,
+        child: FlatButton(
+          color: Colors.cyanAccent[400],
+          padding: EdgeInsets.symmetric(vertical: 28),
+          child: Text(
+            text,
+            style: TextStyle(
+                color: Colors.white, fontFamily: 'Courgette', fontSize: 18),
+          ),
+          onPressed: () {
+            launch(
+                'https://www.tiktok.com/@magic_moments2021?_d=secCgYIASAHKAESMgowdQ76oLewcj1qEK5C89hMtdhh1CJbhO1KfcjIKnnL3bIq%2BC5oHautbYfMgkY0EuuAGgA%3D&checksum=&language=es&preview_pb=0&sec_user_id=MS4wLjABAAAAZbYJjTVSPamNSOGeNuG0DYK91Aty1_tVJYomsdoqU6faeIV-OIj5jGGmCseAvGj_&share_app_id=1233&share_item_id=6962314700180688134&share_link_id=cf8bdae1-a430-4900-b739-6573e345318e&source=h5_m&timestamp=1621258304&u_code=a28k7id502gkl&user_id=221419910399406080&utm_campaign=client_share&utm_medium=android&utm_source=copy&_r=1&lang=es');
           },
         ),
       ),
